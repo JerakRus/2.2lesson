0cc4c97fd4b3c0aea277427f1ecd800fb00cbc80
tritonmind <tritonmind@tmd.local>
2018-09-24 00:08:09 +0300

    lesson2

---
+++ Template/src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DataDevelopers from "./app/components/DataDevelopers"
+
+ReactDOM.render(
+    <div>
+	    <DataDevelopers />
+    </div>,
+	document.getElementById("app")
+);
---
+++ Template/src/app/components/DataDevelopers.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class DataDevelopers extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.showTime = this.showTime.bind(this);
+    }
+
+    showTime = () => {
+        const time = new Date()
+        alert(time);
+    }
+
+    render() {
+        return (
+			<div>
+				<h1>Name:</h1>
+                <h1>Age: </h1>
+                <button onClick={this.showTime}>Click me</button>
+			</div>
+        )
+    }
+}
+
+export default DataDevelopers;
